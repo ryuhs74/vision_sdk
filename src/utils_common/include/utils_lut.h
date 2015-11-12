6961977cd1d4fcd79707d50d5aa357659b125c8a
ryuhs74 <ryuhs74@gmail.com>
2015-11-12 10:46:28 +0900

    VIEW MODE Enum, Struct 추가

--- src/utils_common/include/utils_lut.h
+++ src/utils_common/include/utils_lut.h
@@ -8,7 +8,28 @@
 #ifndef EXAMPLES_TDA2XX_SRC_DEVICES_UTILS_LUT_H_
 #define EXAMPLES_TDA2XX_SRC_DEVICES_UTILS_LUT_H_
 
+//ryuhs74@20151112 - Add View Mode ENUM & STRUCT
+typedef enum
+{
+	TOP_VIEW,
+	FULL_VIEW
+}VIEW_MODE;
 
+typedef enum
+{
+	FRONT_VIEW,
+	REAR_VIEW,
+	LEFT_VIEW,
+	RIGHT_VIEW
+}VIEW_NT;
+
+typedef struct
+{
+	VIEW_MODE viewmode;
+	VIEW_NT viewnt;
+	VIEW_NT	prvVient;
+}ViewMode;
+// - END
 
 
 typedef enum lut_index
