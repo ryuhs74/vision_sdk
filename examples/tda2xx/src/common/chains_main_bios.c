cc43f4a2fa2ec270585dfcdb29f0bbaa5218527b
ryuhs74 <ryuhs74@gmail.com>
2015-11-20 17:11:37 +0900

    Start_AVM_E500 함수 활성

--- examples/tda2xx/src/common/chains_main_bios.c
+++ examples/tda2xx/src/common/chains_main_bios.c
@@ -48,7 +48,7 @@
  */
 //#define CHAINS_DISABLE_GET_CHAR
 
-#define AVM_E500_SV		//ryuhs74@20151110 - Add Change Multi Cam or SV
+//#define AVM_E500_SV		//ryuhs74@20151110 - Add Change Multi Cam or SV
 Chains_Ctrl gChains_usecaseCfg;
 
 /**
@@ -1725,7 +1725,7 @@ Void Chains_main(UArg arg0, UArg arg1)
             char ch;
             Bool done;
 
-            //Start_AVM_E500();
+            Start_AVM_E500();
 
             done = FALSE;
 
